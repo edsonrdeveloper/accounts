67942eb87eba08e94404304ec0849d8f4220faf1
Edson Rodrigues <edsssonrodrigues@gmail.com>
2025-06-16 16:22:04 -0300

    initial structure

---
+++ index.js
@@ -0,0 +1,33 @@
+// módulos externos
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+// módulos internos
+const fs = require("fs");
+const { log } = require("console");
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que você deseja fazer?",
+        choices: [
+          "Criar Conta",
+          "Consultar Saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer["action"];
+
+      console.log(action);
+    })
+    .catch((err) => console.log(err));
+}
+
+operation();
